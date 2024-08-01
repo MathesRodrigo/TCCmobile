5e67d1a741c6295f67046005f9417db681b2d9ff
GabrielVasconcelo5 <gabrvasco2@gmail.com>
2024-08-01 08:16:40 -0300

    init

---
+++ app/src/main/java/com/fieb/aula/acadaemia/tela_de_login.java
@@ -0,0 +1,14 @@
+package com.fieb.aula.acadaemia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tela_de_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_de_login);
+    }
+}
