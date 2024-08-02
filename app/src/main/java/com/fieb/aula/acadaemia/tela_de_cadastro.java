e0125c1d47364b331fcfece57cdecf16345c47de
GabrielVasconcelo5 <gabrvasco2@gmail.com>
2024-08-02 08:20:33 -0300

    meu amigo querido

---
+++ app/src/main/java/com/fieb/aula/acadaemia/tela_de_cadastro.java
@@ -0,0 +1,14 @@
+package com.fieb.aula.acadaemia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tela_de_cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_de_cadastro);
+    }
+}
